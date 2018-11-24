b63c92ca86818e4b613c5cba2ab2f96d978b952a
fajar <fajarfathirwan97@gmail.com>
2018-11-25 03:31:54 +0700

    datatable user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,30 @@
 class UserController < ApplicationController
   before_action :authenticate_user!
+
+  def column
+    @column = ["full_name", "email"]
+  end
+
   def index
-    
+    @users = User.limit(10)
+    @dataTableUrl = route_for(:userDatatable)
+  end
+
+  def datatable
+    datas = User.select([
+      "id",
+      "email",
+      "CONCAT(first_name,' ',last_name) as full_name",
+    ]).limit(params[:length]).offset(params[:start])
+      .order(self.column[params["order"]["0"]["column"].to_i] + " #{params["order"]["0"]["dir"]}")
+    if params[:field] != "" && params[:operator] != ""
+      datas = datas.where(["#{params[:field]} #{params[:operator]} ?", "%#{params[:q]}%"])
+    end
+    render json: {
+      data: datas,
+      draw: params[:draw],
+      recordsFiltered: 0,
+      recordsTotal: User.count(),
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'admin#index'
+  root "admin#index"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/users', to: 'user#index', as: :userIndex
+  get "/users", to: "user#index", as: :userIndex
+  get "/users-datatable", to: "user#datatable", as: :userDatatable
 end
